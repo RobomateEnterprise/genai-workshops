ef27c3c15f0367af7b539264785cf9de7a4b7728
Alex Koniushenko <alex.koniushenko@gmail.com>
2025-07-06 16:16:38 +0300

    Update: terraform setup tested and updated

--- Building GenAI application with AWS Workshop/provider.tf
+++ Building GenAI application with AWS Workshop/provider.tf
@@ -1,3 +1,11 @@
 provider "aws" {
-  region = var.aws_region
+  region     = "us-east-1"                      # ← your region
+  access_key = "..."            # ← your AWS Access Key
+  secret_key = "..."  # ← your AWS Secret Key
+}
+
+provider "awscc" {
+  region     = "us-east-1"
+  access_key = "..."
+  secret_key = "..."
 }
---
+++ Building GenAI application with AWS Workshop/terraform.tfvars
@@ -0,0 +1,2 @@
+vector_database_bedrock_kb_username = "bedrock_user"
+vector_database_bedrock_kb_password = "bedrock_user"
